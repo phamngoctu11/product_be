45b1a7f74689c73df9ffc2f099987e0c05226fe4
phamngoctu <tux2k4@hmail.com>
2026-03-05 11:49:20 +0700

    update voucher

--- src/main/java/com/example/workflow/dto/OrderDTO.java
+++ src/main/java/com/example/workflow/dto/OrderDTO.java
@@ -1,4 +1,5 @@
 package com.example.workflow.dto;
+import com.example.workflow.entity.VoucherTemplate;
 import com.example.workflow.nume.OrderStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,6 +16,10 @@ public class OrderDTO implements Serializable {
     private Long user_id;
     private List<OrderItemDTO> items;
     private double totalPrice;
+    private double discountAmount;  // Số tiền được giảm
+    private double finalPrice;      // Thực trả
+
+    private String voucherName;
     LocalDateTime startOrderTime;
     LocalDateTime endOrderTime;
     OrderStatus status;
--- src/main/java/com/example/workflow/mapper/OrderMapper.java
+++ src/main/java/com/example/workflow/mapper/OrderMapper.java
@@ -3,9 +3,11 @@ import com.example.workflow.dto.OrderDTO;
 import com.example.workflow.entity.Order;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-@Mapper(componentModel = "spring",uses = {OrderItemMapper.class})
+
+@Mapper(componentModel = "spring", uses = {OrderItemMapper.class})
 public interface OrderMapper {
-    @Mapping(source="user.id",target = "user_id")
-    @Mapping(source="id",target = "id")
+    @Mapping(source="user.id", target = "user_id")
+    @Mapping(source="id", target = "id")
+    @Mapping(source="userVoucher.template.name", target = "voucherName")
     OrderDTO toDto(Order order);
 }
